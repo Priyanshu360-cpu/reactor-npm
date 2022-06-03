bdf81948298defa3ae223cca761be291be00eb2e
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-04 03:52:50 +0530

    removed styling and made it user dependent

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   
   
   render(){return (
-   <><div className="trial"><Clock></Clock></div><Test></Test></>
+   <><Clock></Clock><Test></Test></>
   )}
 }
 export default App;
