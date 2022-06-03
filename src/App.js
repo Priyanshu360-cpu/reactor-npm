a3a0ce78c3efb2374f54230391cf729d34fc0cea
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-04 03:06:12 +0530

    updated source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
+import Clock from './clock';
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      seconds:"0",
-    };
-    this.clock=this.clock.bind(this);
-  }
-  clock(handler){
-    setInterval(()=>{this.setState({seconds:(parseInt(this.state.seconds)+1).toString()})},1000);
-
-  }
+  
+  
   render(){return (
-    <div>{this.state.seconds}
-   <this.clock/> 
-   </div>
+   <Clock></Clock>
   )}
 }
 export default App;
