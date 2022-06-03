6c6caa9747e74265083b4709e08423b586fd5f39
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-04 02:38:19 +0530

    component initalised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      seconds:"0",
+    };
+    this.clock=this.clock.bind(this);
+  }
+  clock(handler){
+setInterval(()=>{this.setState({seconds:(parseInt(this.state.seconds)+1).toString})},1000);
+  }
+  render(){return (
+    <h1>{this.state.seconds}
+   <this.clock/> 
+   </h1>
+  )}
 }
-
 export default App;
